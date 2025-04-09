cd10378123cabb2a582837b9304cdf71417683cf
jayasree161997 <jayasrees317@gmail.com>
2025-04-09 21:52:38 +0530

    Update settings.py

--- Ecommerce/Ecommerce/settings.py
+++ Ecommerce/Ecommerce/settings.py
@@ -207,12 +207,21 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+#STATIC_URL = 'static/'
 # STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_ROOT = '/home/ubuntu/woodora-ecommerce/staticfiles'
-STATICFILES_DIRS=[
+#STATIC_ROOT = '/home/ubuntu/woodora-ecommerce/staticfiles'
+#STATICFILES_DIRS=[
     # os.path.join(BASE_DIR,'static')
- '/home/ubuntu/woodora-ecommerce/Ecommerce/static',
+ #'/home/ubuntu/woodora-ecommerce/Ecommerce/static',
+
+#]
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
 
 ]
 MEDIA_URL = '/media/'
